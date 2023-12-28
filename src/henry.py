a7443599bacb14195cb756db49913257311b5ba9
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-12-28 17:20:53 +0500

    Update src/henry.py
    Co-authored-by: Henry R. <87513666+henry-richard@users.noreply.github.com>

--- src/henry.py
+++ src/henry.py
@@ -2,6 +2,6 @@ print("Henry testing v0.8.0")
 print("Henry testing v0.8.0")
 print("Henry testing v0.8.0")
 print("Wandaaaa testing v0.8.0")
-print("Henry testing v0.8.01")
+print("Henry testing v0.8.21")
 print("Wandaaaa testing v0.8.0")
 
