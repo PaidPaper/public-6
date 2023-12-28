a2ffb8174de690129e09632c73289f5a56a92052
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-12-28 17:13:19 +0500

    Update henry.py

--- src/henry.py
+++ src/henry.py
@@ -2,4 +2,6 @@ print("Henry testing v0.8.0")
 print("Henry testing v0.8.0")
 print("Henry testing v0.8.0")
 print("Wandaaaa testing v0.8.0")
+print("Henry testing v0.8.0")
+print("Wandaaaa testing v0.8.0")
 
